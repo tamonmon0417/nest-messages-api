a74d7d840522cbbf5f93f4eb4ebac5cc6b5f4882
tamonmon0417 <tamonmon0417@gmail.com>
2022-03-25 22:55:23 +0900

    add dto

---
+++ src/messages/dtos/create-message.dto.ts
@@ -0,0 +1,6 @@
+// data transfer object
+import { IsString } from 'class-validator';
+export class CreateMessageDto {
+  @IsString()
+  content: string;
+}
--- src/messages/messages.controller.ts
+++ src/messages/messages.controller.ts
@@ -1,13 +1,17 @@
-import { Controller, Get, Post } from '@nestjs/common';
-
+import { Controller, Get, Post, Body, Param } from '@nestjs/common';
+import { CreateMessageDto } from './dtos/create-message.dto';
 @Controller('messages')
 export class MessagesController {
   @Get()
   listMessages() {}
 
   @Post()
-  createMessage() {}
+  createMessage(@Body() body: CreateMessageDto) {
+    console.log(body);
+  }
 
   @Get('/:id')
-  getMessage() {}
+  getMessage(@Param('id') id: string) {
+    console.log(id);
+  }
 }
