7b4254a2cf268b90d064707933ca9ba64b725d0e
tamonmon0417 <tamonmon0417@gmail.com>
2022-03-26 09:17:58 +0900

    add error and exception

--- src/messages/messages.service.ts
+++ src/messages/messages.service.ts
@@ -2,6 +2,7 @@ import { MessagesRepository } from './messages.repository';
 
 export class MessagesService {
   messagesRepo: MessagesRepository;
+
   constructor() {
     // Service is creating its own dependencies
     // DONT DO THIS ON REAL APPS
